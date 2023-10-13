5bcc771e07b8a81936bc08db7ea45601a36f3eff
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-10-13 18:32:00 +0530

    added id to df

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,23 +1,18 @@
-import tweepy
+import snscrape.modules.twitter as sntwitter
 
-USER_KEY = "lXKBvt2L8Q6pAh2BJWeiHwyGy"
-USER_KEY_SECRET = "lqjeJKG3SvBwNEOpbKbr6o3LwHRUEHkfIU9wr25NzFlT2KUiJL"
+# Replace the tweet ID below with the ID of the tweet you want to scrape
+tweet_id = 1513639102339588099
 
-ACCESS_TOKEN = "1460465110992883714-nuQy76o6KlL7sUMEVbo1VvAVreV9IF"
-ACCESS_TOKEN_SECRET = "i3c0jNkao3grtYSzno5bApOQTRL1dIru3eiIyqssHrAR0"
+# Construct a URL for the tweet using its ID
+url = f'https://twitter.com/GazetaRu/status/{tweet_id}'
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(USER_KEY, USER_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+tweet_list = []
 
-# Create API object
-api = tweepy.API(auth)
+# Scrape the tweet using its URL
+print(sntwitter.TwitterSearchScraper(f'url:'+url).get_items())
 
-# Tweet ID to scrape
-tweet_id = "1498705460060430343"
+tweets = sntwitter.TwitterSearchScraper(f'url:'+url).get_items()
 
-# Scrape tweet by ID
-tweet = api.get_status(tweet_id)
-
-# Print tweet text
-print(tweet.text)
+for tweet in tweets:
+    # print(tweet.content)
+   print(tweet)

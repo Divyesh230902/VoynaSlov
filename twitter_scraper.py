882f30028386c9eb430caece2cf469cd7affd5a2
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-10-21 17:01:30 +0530

    scrapping is not working

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,6 +1,5 @@
-import snscrape.modules.twitter as sntwitter
-
-import tweepy as ts
+import requests
+from bs4 import BeautifulSoup
 
 # Replace the tweet ID below with the ID of the tweet you want to scrape
 tweet_id = 1513639102339588099
@@ -11,10 +10,11 @@ url = f'https://twitter.com/GazetaRu/status/{tweet_id}'
 tweet_list = []
 
 # Scrape the tweet using its URL
-print(sntwitter.TwitterSearchScraper(f'url:'+url).get_items())
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-tweets = sntwitter.TwitterSearchScraper(f'url:'+url).get_items()
+# Find the div with the tweet text
+tweet_text = soup.find('div', class_='css-901oao css-bfa6kz r-111h2gw r-18u37iz r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-qvutc0')
 
-for tweet in tweets:
-    # print(tweet.content)
-   print(tweet)
+# Print the tweet text
+print(tweet_text.text)

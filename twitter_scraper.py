ded5db9585617f6e29441877d252eb34d244c217
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-10-21 18:16:33 +0530

    final scraping

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,8 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+tweets = os.walk('Twitter')
+tweet_dir = {
+    'root': [],
+    'dirs': [],
+    'tweet_id': []
+}
+print(tweets)
+for root, dirs, files in tweets:
+    tweet_dir['dirs'].append(root)
+    for file in files:
+        tweet_dir['root'].append(file)
+        print(file)
+        with open(os.path.join(root, file), 'r') as f:
+            print(f.read())
+            tweet_dir['tweet_id'].append(f.read())
 
 # Replace the tweet ID below with the ID of the tweet you want to scrape
-tweet_id = 1513639102339588099
+tweet_id = tweet_dir['tweet_id'][0]
 
 # Construct a URL for the tweet using its ID
 url = f'https://twitter.com/GazetaRu/status/{tweet_id}'
